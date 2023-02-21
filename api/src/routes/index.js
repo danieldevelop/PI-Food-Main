0622d2bb4900abee3016dc08a8cbc4f7be04e0b2
Daniel Gomez <dgomez.informatica@gmail.com>
2023-02-21 13:28:09 -0300

    add configuration to routes general in recipes and diets

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,16 @@
 const { Router } = require('express');
+// const morgan = require('morgan');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-
+const { recipeController} = require('../controllers/recipeController');
+const { dietController } = require('../controllers/dietController')
 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
+router.use('/recipes', recipeController);
+router.use('/diets', dietController);
 
 
 module.exports = router;
