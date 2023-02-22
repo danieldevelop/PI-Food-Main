d89d8a1f6e3a0b01e61cfe741cf1f0a870dc14b2
Daniel Gomez <dgomez.informatica@gmail.com>
2023-02-22 20:04:11 -0300

    refactor: funcion getRecetaById, con una de prueba

--- api/src/controllers/recipeController.js
+++ api/src/controllers/recipeController.js
@@ -3,33 +3,57 @@ const { Recipe } = require('../db');
 
 require('dotenv').config();
 
-// add despues param id
-const getRecetaById = async () => {
-    let recipeDetail = {};
+const getRecetaById = async (id) => {
+    let recetaDetail = {};
 
     try {
-        // let recipe = await axios.get(`https://api.spoonacular.com/recipes/${id}/information?apiKey=${process.env.API_KEY}`);
-        let recipe = await axios.get('https://run.mocky.io/v3/84b3f19c-7642-4552-b69c-c53742badee5');
+        const recetas = await axios.get(`https://api.spoonacular.com/recipes/${id}/information?apiKey=${process.env.API_KEY}`);
+        const recetaId = recetas.data.id;
+        // const recetas = await axios.get('https://run.mocky.io/v3/84b3f19c-7642-4552-b69c-c53742badee5');
+        // const recetaId = recetas.data.results.id;
 
-        recipeDetail = {
-            id: recipe.data.id,
-            title: recipe.data.title,
-            image: recipe.data.image,
-            summary: recipe.data.summary,
-            healthScore: recipe.data.healthScore,
-            // Ingredients: recipe.data.extendedIngredients.map(ingredient => {
-            //     return { name: ingredient.name, amount: ingredient.amount, unit: ingredient.unit };
-            // }),
-            Steps: recipe.data.analyzedInstructions[0].steps.map(step => {
-                return { number: step.number, step: step.step };
-            })
+        // ESTA FORMA ES LA API
+        recetaDetail = {
+            id: recetas.data.id,
+            title: recetas.data.title,
+            name: recetas.data.name,
+            image: recetas.data.image,
+            summary: recetas.data.summary,
+            healthScore: recetas.data.healthScore,
+            steps: recetas.data.analyzedInstructions.map(step => step.steps.map(step => {
+                return ({
+                    number: step.number,
+                    step: step.step
+                });
+            }
+            ))
         }
-        return recipeDetail;
 
+        // ESTA FORMA ES EL MOCKY
+        // recetaDetail = recetas.data.results.map(receta => {
+        //     return ({
+        //         id: receta.id,
+        //         title: receta.title, 
+        //         name: receta.name,
+        //         image: receta.image,
+        //         summary: receta.summary,
+        //         healthScore: receta.healthScore,
+        //         steps: receta.analyzedInstructions.map(step => step.steps.map(step => {
+        //             return ({
+        //                 number: step.number,
+        //                 step: step.step
+        //             });
+        //         }))
+        //     });
+        // })
+
+        return recetaDetail;
     } catch (err) {
         console.log(`${err}`);
     }
 }
+
+
 
 const getRecetaByName = async (name) => {
     const number = 100; // cantidad de registros a mostrar
