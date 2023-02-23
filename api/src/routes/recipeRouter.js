a3d4ebfe972a0b58a078f2363bcc027540be9d15
Daniel Gomez <dgomez.informatica@gmail.com>
2023-02-23 20:28:02 -0300

    add ruta post, para agregar data a database, (hay no terminado)

--- api/src/routes/recipeRouter.js
+++ api/src/routes/recipeRouter.js
@@ -1,5 +1,9 @@
 const recetaRouter = require('express').Router();
-const { getRecetaById, getRecetaByName } = require('../controllers/recipeController');
+const { 
+    getRecetaById, 
+    getRecetaByName, 
+    setRecetaDatabase 
+} = require('../controllers/recipeController');
 
 
 // GET | /recipes/:idRecipe
@@ -28,6 +32,24 @@ recetaRouter.get('/', async (req, res) => {
         (receta.length > 0) 
             ? res.status(200).json(receta)
             : res.status(404).json({ message: `no se encontro la receta: ${name}`})
+    }
+});
+
+
+// POST | /recipes (lo que recibo para guardarlos en la database)
+recetaRouter.post('/', async (req, res) => {
+    const { name, image, summary, healthscore, steps } = req.body;
+
+    try {
+        // !error en el if
+        // if (!name || !image || !summary || !healthscore || !steps) throw Error('Me falta información');
+        // else {
+            const newRecipe = await setRecetaDatabase(name, image, summary, healthscore, steps);
+            return res.status(200).json(newRecipe);
+        // }
+
+    } catch (err) {
+        res.status(404).json(err)
     }
 });
 
