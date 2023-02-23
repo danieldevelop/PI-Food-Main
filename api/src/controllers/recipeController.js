95f87def5944e76e7272a62d00fce1fae641f264
Daniel Gomez <dgomez.informatica@gmail.com>
2023-02-23 16:55:49 -0300

    refactor: funcion getRecetaByName, con datos de prueba provisorios

--- api/src/controllers/recipeController.js
+++ api/src/controllers/recipeController.js
@@ -4,13 +4,11 @@ const { Recipe } = require('../db');
 require('dotenv').config();
 
 const getRecetaById = async (id) => {
-    let recetaDetail = {};
+    let recetaDetail = undefined;
 
     try {
         const recetas = await axios.get(`https://api.spoonacular.com/recipes/${id}/information?apiKey=${process.env.API_KEY}`);
-        const recetaId = recetas.data.id;
         // const recetas = await axios.get('https://run.mocky.io/v3/84b3f19c-7642-4552-b69c-c53742badee5');
-        // const recetaId = recetas.data.results.id;
 
         // ESTA FORMA ES LA API
         recetaDetail = {
@@ -46,8 +44,8 @@ const getRecetaById = async (id) => {
         //         }))
         //     });
         // })
-
         return recetaDetail;
+
     } catch (err) {
         console.log(`${err}`);
     }
@@ -56,28 +54,29 @@ const getRecetaById = async (id) => {
 
 
 const getRecetaByName = async (name) => {
-    const number = 100; // cantidad de registros a mostrar
+    const number = 2; // cantidad de registros a mostrar
     let recipes = [];
 
     try {
         let recipe = await axios.get(`https://api.spoonacular.com/recipes/complexSearch?query=${name}&number=${number}&apiKey=${process.env.API_KEY}`);
+
         recipes = recipe.data.results.map(recipe => {
             return {
                 id: recipe.id,
                 title: recipe.title,
-                image: recipe.image,
-                summary: recipe.summary,
-                healthScore: recipe.healthScore,
-                diets: recipe.diets
+                image: recipe.image
             }
-        })
+        });
         return recipes;
+
     } catch(err) {
         console.log(`${err}`);
     }
 }
 
-// POST
+
+
+// POST database
 const setRecetaDatabase = async (title, image, summary, healthScore, steps) => {
     // Creating new recipe
     const [newRecipe, created] = await Recipe.findOrCreate({
