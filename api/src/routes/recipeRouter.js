7f8c63af98683f77a67694a043a92041dcceaab4
Daniel Gomez <dgomez.informatica@gmail.com>
2023-02-23 16:52:00 -0300

    add ruta para buscar por nombre de receta

--- api/src/routes/recipeRouter.js
+++ api/src/routes/recipeRouter.js
@@ -1,5 +1,5 @@
 const recetaRouter = require('express').Router();
-const { getRecetaById } = require('../controllers/recipeController');
+const { getRecetaById, getRecetaByName } = require('../controllers/recipeController');
 
 
 // GET | /recipes/:idRecipe
@@ -13,7 +13,24 @@ recetaRouter.get('/:id', async (req, res) => {
             ? res.status(404).json({ error: "no se encontro la receta" })
             : res.status(200).json(receta);
     }
-})
+});
+
+
+// GET | /recipes/name?="..."
+recetaRouter.get('/', async (req, res) => {
+    const { name } = req.query;
+
+    if (name) {
+        const receta = await getRecetaByName(name);
+        console.log(receta.length);
+
+        //!NOTA: EL ORDEN DE LA COSAS IMPORTANTE
+        (receta.length > 0) 
+            ? res.status(200).json(receta)
+            : res.status(404).json({ message: `no se encontro la receta: ${name}`})
+    }
+});
+
 
 
 module.exports = recetaRouter;
