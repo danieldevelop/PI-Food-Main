176a537804e983eab54aad71fd78056e19ad9fcb
Daniel Gomez <dgomez.informatica@gmail.com>
2023-03-10 22:21:32 -0300

    add ruta para extraer las dietas de las recetas

--- api/src/routes/dietRouter.js
+++ api/src/routes/dietRouter.js
@@ -1,4 +1,12 @@
 const dietRouter = require('express').Router();
+const {
+    getDiets
+} = require('../controllers/dietController');
+
+// GET | /diets
+dietRouter.get("/", async (req, res) => {
+
+});
 
 
 module.exports = dietRouter;
