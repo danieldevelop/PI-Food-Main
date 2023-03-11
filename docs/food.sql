752eb289617e492e8a5ba82f1e62c74277010099
Daniel Gomez <dgomez.informatica@gmail.com>
2023-03-10 22:30:18 -0300

    respaldo database PI-food

---
+++ docs/food.sql
@@ -0,0 +1,11 @@
+-- PI Proyecto Individual
+-- Proyect PI-Food-main Henry
+-- USER DAFAULT DATABASE postgres
+
+CREATE DATABASE food;
+
+SELECT * FROM recipes r;
+
+SELECT * FROM diets d;
+
+SELECT * FROM recipe_has_diet;
